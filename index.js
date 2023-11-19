3dd282f2391ecc8972fb6ea8b917c9c44618a0fc
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2023-11-19 18:23:10 +0100

    Winning check Implement a check for winning using horizontal cells

--- index.js
+++ index.js
@@ -9,4 +9,26 @@ const ttt = (() => {
     };
     return { generateBoard, turn, board };
   })();
+
+  const check = (() => {
+    let win = null;
+    const horizontalCheck = () => {
+      for (let i = 0; i < gameBoard.board.length; i++) {
+        gameBoard.board[i].every((element) => element === gameBoard.board[i][0])
+          ? (check.win = true)
+          : (check.win = false);
+        console.log(check.win);
+        if (check.win == true) {
+          break;
+        }
+      }
+    };
+    return { horizontalCheck, win };
+  })();
+
+  gameBoard.generateBoard();
+  for (let i = 0; i <= 2; i++) {
+    gameBoard.board[1][i] = "g";
+  }
+  check.horizontalCheck();
 })();
