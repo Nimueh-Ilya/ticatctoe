14e8bee9c7f5bd09edd585e2b5c5571c31d6e8a4
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2023-11-20 18:31:08 +0100

    Implement all checks for winning and tie checker

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const ttt = (() => {
         gameBoard.board[i].every((element) => element === gameBoard.board[i][0])
           ? (check.win = true)
           : (check.win = false);
-        if (check.win == true) {
+        if (check.win === true) {
+          console.log("winner 1");
           break;
         }
       }
@@ -28,24 +29,62 @@ const ttt = (() => {
         for (let j = 0; j < gameBoard.board.length; j++) {
           tempArray.push(gameBoard.board[j][i]);
         }
-        console.log(tempArray);
         tempArray.every((element) => element === tempArray[0])
           ? (check.win = true)
           : (check.win = false);
-        if (check.win == true) {
+        if (check.win === true) {
+          console.log("winner 2");
           break;
         }
         tempArray = [];
       }
     };
-    const diagonalCheck = () => {};
-    return { verticalCheck, diagonalCheck, horizontalCheck, win };
+    const diagonalCheck = () => {
+      let tempArray = [];
+      tempArray.push(
+        gameBoard.board[0][0],
+        gameBoard.board[1][1],
+        gameBoard.board[2][2]
+      );
+      if (tempArray.every((element) => element === tempArray[0]) === true) {
+        check.win = true;
+        console.log("winner 3");
+      } else {
+        tempArray = [];
+        tempArray.push(
+          gameBoard.board[0][2],
+          gameBoard.board[1][1],
+          gameBoard.board[2][0]
+        );
+        if (tempArray.every((element) => element === tempArray[0]) === true) {
+          check.win = true;
+          console.log("winner 4");
+        }
+      }
+    };
+    const checkAll = () => {
+      if (gameBoard.turn == 9) {
+        console.log("tie");
+      } else {
+        check.win = null;
+        verticalCheck();
+        check.win = null;
+        horizontalCheck();
+        check.win = null;
+        diagonalCheck();
+        check.win = null;
+      }
+    };
+    return { checkAll, win };
   })();
 
-  // gameBoard.generateBoard();
-  // for (let i = 0; i <= 2; i++) {
-  //   gameBoard.board[1][i] = 1;
-  // }
-  // console.log(gameBoard.board);
-  // check.verticalCheck();
+  gameBoard.generateBoard();
+
+  gameBoard.board[0][0] = 3;
+  gameBoard.board[1][1] = 3;
+  gameBoard.board[2][2] = 3;
+  console.log(gameBoard.board);
+  gameBoard.turn = 9;
+  check.checkAll();
+  console.log(check.win);
 })();
