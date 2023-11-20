727d0d2434e6a1a0ef960fdc30fa06061233a87e
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2023-11-20 13:49:59 +0100

    Add vertical check to win check

--- index.js
+++ index.js
@@ -11,24 +11,41 @@ const ttt = (() => {
   })();
 
   const check = (() => {
-    let win = null;
+    const win = null;
     const horizontalCheck = () => {
       for (let i = 0; i < gameBoard.board.length; i++) {
         gameBoard.board[i].every((element) => element === gameBoard.board[i][0])
           ? (check.win = true)
           : (check.win = false);
-        console.log(check.win);
         if (check.win == true) {
           break;
         }
       }
     };
-    return { horizontalCheck, win };
+    const verticalCheck = () => {
+      let tempArray = [];
+      for (let i = 0; i < gameBoard.board.length; i++) {
+        for (let j = 0; j < gameBoard.board.length; j++) {
+          tempArray.push(gameBoard.board[j][i]);
+        }
+        console.log(tempArray);
+        tempArray.every((element) => element === tempArray[0])
+          ? (check.win = true)
+          : (check.win = false);
+        if (check.win == true) {
+          break;
+        }
+        tempArray = [];
+      }
+    };
+    const diagonalCheck = () => {};
+    return { verticalCheck, diagonalCheck, horizontalCheck, win };
   })();
 
-  gameBoard.generateBoard();
-  for (let i = 0; i <= 2; i++) {
-    gameBoard.board[1][i] = "g";
-  }
-  check.horizontalCheck();
+  // gameBoard.generateBoard();
+  // for (let i = 0; i <= 2; i++) {
+  //   gameBoard.board[1][i] = 1;
+  // }
+  // console.log(gameBoard.board);
+  // check.verticalCheck();
 })();
